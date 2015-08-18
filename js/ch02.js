cad9228a9731c31c81bd83aa4f0ac215984d48ce
rogilesor <istarosel@yahoo.fr>
2015-08-18 14:00:44 +0200

    apres ajout de this

--- js/ch02.js
+++ js/ch02.js
@@ -10,10 +10,16 @@ function createClock(id){
 		c.updateClock = function(){
 			var date = new Date();		
 			var clock = document.getElementById(id);
-			clock.innerHTML = formatDigits(date.getHours())+" : "+formatDigits(date.getMinutes())+ " : "+ formatDigits(date.getSeconds());
+			clock.innerHTML = this.formatDigits(date.getHours())+" : "+this.formatDigits(date.getMinutes())+ " : "+ this.formatDigits(date.getSeconds());
 		}
 
-	setInterval(c.updateClock,1000);
+	c.formatDigits = function(val){
+		if (val<10) val="0" + val ;
+		return val ;
+	}
+
+
+	setInterval(function(){c.updateClock();},1000);
 	c.updateClock();
 
 	return c ;
@@ -31,10 +37,7 @@ function updateClock(){
 	clock.innerHTML = date.getHours()+" : "+date.getMinutes()+ " : "+ date.getSeconds();
 }
 
-function formatDigits(val){
-	if (val<10) val="0" + val ;
-	return val ;
-}
+
 
 window.onload = onReady;
 
