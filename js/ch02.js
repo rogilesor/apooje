2e0223f0a068129aff394aa6273e2ff320863d59
rogilesor <istarosel@yahoo.fr>
2015-08-19 21:59:40 +0200

    avant video 3.3

--- js/ch02.js
+++ js/ch02.js
@@ -1,32 +1,32 @@
 function onReady(){
 	console.log('Hello Chapter 2');
 
-	var clock1  = new Clock('clock1',0,'ETC');
+	var clock1  = new Clock('clock1',0,'');
 	var clock2 = new Clock('clock2',120);
 
-	
+}
 
+Date.prototype.updateSeconds = function(){
+	this.setSeconds(this.getSeconds()+1);
+}
 
+Date.prototype.autoclock = function(isAuto){
+	clearInterval(this.clockInterval);
+	if(isAuto){
+		var that = this ;
+		this.clockInterval = setInterval(function(){that.updateSeconds()},1000);
+	}
 }
 
 function Clock(id, offset,label){
 	offset = offset || 0 ;
-	var d = new Date() ;
-	this.offset = (offset + d.getTimezoneOffset())*60*1000;
 	label = label || '' ;
-
-	this.updateClock = function(){
-//			console.log('this.update actionne')
-			var date = new Date();		
-				date = new Date(this.offset + date.getTime());
-			var clock = document.getElementById(id);
-			clock.innerHTML = this.formatDigits(date.getHours())+" : "+this.formatDigits(date.getMinutes())+ " : "+ this.formatDigits(date.getSeconds())+" "+label;
-	}
-
-	this.formatDigits = function(val){
-		if (val<10) val="0" + val ;
-		return val ;
-	}
+	var d = new Date() ;
+	var offset = (offset + d.getTimezoneOffset())*60*1000;
+	this.d = new Date(offset+d.getTime());
+	this.d.autoclock(true);
+	this.id = id ;
+	this.label = label ;
 
 	var that = this ;
 	setInterval(function(){
@@ -36,19 +36,19 @@ function Clock(id, offset,label){
 
 }
 
-function updateClock(){
-	console.log('update en dehors de la classe')
-	var date = new Date();
-	//console.log(date);
-	//console.log(date.getHours());
-	//console.log(date.getMinutes());
-	//console.log(date.getSeconds());
-	
-	var clock = document.getElementById('clock');
-	//console.log(clock);
-	clock.innerHTML = date.getHours()+" : "+date.getMinutes()+ " : "+ date.getSeconds();
+Clock.prototype.updateClock = function(){
+//			console.log('this.update actionne')
+		var date = this.d ;
+			//date.updateSeconds();		
+		var clock = document.getElementById(this.id);
+		clock.innerHTML = this.formatDigits(date.getHours())+" : "
+		+this.formatDigits(date.getMinutes())+ " : "+ this.formatDigits(date.getSeconds())+" "+this.label;
 }
 
+Clock.prototype.formatDigits = function(val){
+	if (val<10) val="0" + val ;
+	return val ;
+}
 
 
 window.onload = onReady;
